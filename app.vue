dffff3553bddf8a21a70f19dc2efb15744577cd3
sohilpro <sohil123dhh@gmail.com>
2025-06-04 11:00:40 +0330

    re push project

--- app.vue
+++ app.vue
@@ -12,7 +12,6 @@ useHead({
     return productCategory ? `${productCategory} | Cut Size` : "Cut Size";
   },
   meta: [
-    // { name: "viewport", content: "width=device-width, initial-scale=1.0" },
     { name: "viewport", content: "width=1280" },
   ],
   link: [{ rel: "icon", type: "image/svg+xml", href: "/siteicon.svg" }],
